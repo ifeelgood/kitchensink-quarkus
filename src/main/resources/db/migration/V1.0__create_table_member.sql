f11b677bc6d345fbefae66fd9de580aa263e59e5
Vitaliy Baschlykoff <vitaliy.baschlykoff@gmail.com>
2024-05-29 12:31:59 +1000

    Remove not required code and comments

--- src/main/resources/db/migration/V1.0__create_table_member.sql
+++
@@ -1,4 +0,0 @@
-create table Member (id bigint not null, email varchar(255), name varchar(255), phone_number varchar(255), primary key (id));
-alter table if exists Member drop constraint if exists member_uniq_email;
-alter table if exists Member add constraint member_uniq_email unique (email);
-create sequence Member_SEQ start with 1 increment by 1;
